08f5f52c8fb6d1b24e2bc09f70dfb9b0428f5e9b
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-24 04:32:05 +0000

    fixed bug wherin some code was commented out that should not have been commented out

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,11 +4,13 @@ $(document).ready(function () {
     const $this = $(this);
     const $counter = $this.parent().find(".counter");
     const tweetLength = 140 - $this.val().length;
+    $counter.val(tweetLength);
     if (tweetLength < 0) {
       $counter.addClass("counter-red");
     } else {
       $counter.removeClass("counter-red")
     }
   });
+
 });
 
