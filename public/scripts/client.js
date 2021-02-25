cb411f8712ae97ffb925214884b2004425b111a3
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-25 05:35:35 +0000

    created ajax for get/post f(x)s for rendering exisiting tweets and submitting new ones

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -15,31 +15,35 @@
 
 $(document).ready(function () {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+
+  // const $button = $('#post-tweet');
+  // $button.on('click', function () {
+  //   console.log('Button clicked, performing ajax call...');
+  //   $.ajax('index.html', { method: 'POST' })
+  // .then(function () {
+  //   console.log('Success: ', );
+  // });
+  // });
+  $('form').submit(function (event) {
+    event.preventDefault();
+    const localdata = $(this).serialize();
+    $.ajax('/tweets', { method: 'POST', data: localdata })
+      .then(function () {
+        console.log("success!");
+      });
+  });
+  //
+
+  const loadTweets = function () {
+    $.ajax('/tweets', { method: "GET" })
+      .then(function(tweets) {
+        renderTweets(tweets)
+        console.log('tweets loaded! success!', tweets)
+      });
+  };
+  loadTweets();
+
+  //
 
   const renderTweets = function (tweets) {
     for (let tweet of tweets) {
@@ -76,7 +80,7 @@ $(document).ready(function () {
     return $tweet;
   };
 
-  renderTweets(data);
+  // renderTweets(data);
 
 });
 
