a67230ff607b21adaa6394cbbc26b40d61158772
Molly Trepanier <mtrepanier123@gmail.com>
2021-02-26 22:38:21 +0000

    linted client.js, composer-char-counter.js, and ensured dependencies were added to package.json

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,11 +6,14 @@
 
 //
 
-$(document).ready(function () {
+$(document).ready(function() {
   $('form').submit(function (event) {
     event.preventDefault();
+    //
 
-    // checks if tweet is a valid tweet 
+
+
+    // checks if tweet is a valid tweet && sends appropriate error message
 
     console.log($("#tweet-text").val().length);
     if ($("#tweet-text").val().length > 140) {
@@ -35,23 +38,30 @@ $(document).ready(function () {
 
   // 
 
-  const loadTweets = function () {
+  const loadTweets = function() {
     $.ajax('/tweets', { method: "GET" })
       .then(function (tweets) {
         renderTweets(tweets);
         console.log('tweets loaded! success!', tweets);
       });
   };
+  //
+
+  // loads tweets when page is opened-- must live here, under the get and post requests. 
+
   loadTweets();
 
   //
 
-  const renderTweets = function (tweets) {
+  // renders tweets to be inserted in to index.html and then styled  by appropriate style-sheet
+
+
+  const renderTweets = function(tweets) {
     for (let tweet of tweets) {
       $(".tweet-container").prepend(createTweetElement(tweet));
     }
   };
-  const createTweetElement = function (tweet) {
+  const createTweetElement = function(tweet) {
     const safeHTML = `<p>${escape(tweet.content.text)}<p>`;
     const $tweet = `
     <article class = "tweet">
@@ -70,7 +80,7 @@ $(document).ready(function () {
       </div>
       <footer class="tweet-footer">
         <span class="date">${tweet.created_at}</span>
-        <span class="icons"><i class="fas fa-crow"></i></span>
+        <span class="icons"><i class="fa fa-flag"> </i> <i class="fas fa-heart"> </i> <i class="fas fa-crow"> </i></span>
       </footer>
     </article>`;
     return $tweet;
@@ -78,16 +88,19 @@ $(document).ready(function () {
 
   //
 
+  // used to prevent xss attacks
 
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
-  // 
+  //
 
-  const errMessage = function (msg) {
+  // generates error messages; used in tweet validation as callback
+
+  const errMessage = function(msg) {
     let $error = ``;
     if (msg === "too long") {
       $error = `Sorry! This tweet is too long! Please Try Again`;
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,5 @@
+//this f(x) counts the characters of the tweet as the tweet is typed, and displays a number with the remaining tweets
+
 $(document).ready(function () {
   $("#tweet-text").on("input" , function (e) {
     const $counter = $(this).parent().find(".counter");
